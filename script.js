acbfca45082e02463870ee0b57df500efebdfb3e
NikaVerona <46203132+NikaVerona@users.noreply.github.com>
2024-05-20 22:56:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-async function convertCurrency() {
-  const amount = document.getElementById("amount").value;
-  const currency = document.getElementById("currency").value;
+document
+  .getElementById("converter-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-  if (!amount || amount <= 0) {
-    alert("Please enter the correct amount.");
-    return;
-  }
+    const amount = event.target.amount.value;
+    const currency = event.target.currency.value;
 
-  try {
-    const response = await fetch(
-      `https://api.nbp.pl/api/exchangerates/rates/a/${currency}/?format=json`
-    );
-    const data = await response.json();
-    const rate = data.rates[0].mid;
-    const result = (amount * rate).toFixed(2);
+    document.getElementById("result").innerText = "Amount in PLN: -";
+    document.getElementById("error-message").innerText = "";
 
-    document.getElementById("result").innerText = `Amount in PLN: ${result}`;
-  } catch (error) {
-    alert("Error while receiving data. Please try again later.");
-    console.error(error);
-  }
-}
+    if (!amount || amount <= 0) {
+      document.getElementById("error-message").innerText =
+        "Please enter a correct amount.";
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `https://api.nbp.pl/api/exchangerates/rates/a/${currency}/?format=json`
+      );
+      const data = await response.json();
+      const rate = data.rates[0].mid;
+      const result = (amount * rate).toFixed(2);
+
+      document.getElementById("result").innerText = `Amount in PLN: ${result}`;
+    } catch (error) {
+      document.getElementById("error-message").innerText =
+        "Error while receiving data. Please try again later.";
+      console.error(error);
+    }
+  });
